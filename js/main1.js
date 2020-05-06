65f0977b8585f1404eb645c6291b240858ba0bcc
carrerinfo <kumarpranay960@gmail.com>
2020-05-06 18:07:10 +0530

    Update index.html
    update

--- js/main1.js
+++
@@ -1,8 +0,0 @@
-<script type='text/javascript'>
-  window.smartlook||(function(d) {
-    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
-    })(document);
-    smartlook('init', '1a4674afc96374aeee2890d70ccc6e44be1911b1');
-</script>
